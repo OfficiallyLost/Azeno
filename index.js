1920d77a16151890e260f54ed413e0c136119eb1
gavintjhxx <gavintjhx@gmail.com>
2020-07-07 21:32:38 +0800

    Javascript Classes Practice

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("This is the master branch.")
+class Person
+{
+
+    constructor(name, age)
+    {
+
+        if(!name || !age) throw new SyntaxError("Person constructor takes 2 parameters.");
+        if(typeof name !== "string") throw new SyntaxError("Person name must be a string.");
+        if(typeof age !== "number") throw new SyntaxError("Person age must be a number.");
+
+        this.name = name;
+        this.age = age;
+
+    }
+
+    getName() 
+    {
+        return this.name;
+    }
+
+    getAge()
+    {
+        return this.age;
+    }
+
+}
+
+const John = new Person("John", 14);
+console.log(John.getName());
+console.log(John.getAge());
+console.log(John);
